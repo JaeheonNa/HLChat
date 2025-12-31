5c73c2e536c00845fb4cbfa8de1c7af7e4771589
jaeheon.na <jaeheon.na@jaeheonna-MacBook-Pro.local>
2025-12-31 14:10:13 +0900

    HLChat: controller

--- HLChat/src/main.py
+++ HLChat/src/main.py
@@ -1,24 +1,10 @@
-from fastapi import Depends, FastAPI, WebSocket
-
-from schema.request import SendMessageRequest
-from service.messageService import MessageHandler
-from service.messageServiceInterface import MessageHandlerInterface
+from fastapi import FastAPI
+from api import message
 
 app = FastAPI()
 
-@app.post("/HLChat", status_code=200)
-def sendMessage(
-        request: SendMessageRequest,
-        message_handler: MessageHandlerInterface = Depends(MessageHandler)
-):
-    message_handler.handleMessage(request)
+app.include_router(message.router)
 
-@app.websocket("/HLChat/{room_id}")
-async def websocket_endpoint(room_id: str,
-                             websocket: WebSocket,
-                             message_handler: MessageHandlerInterface = Depends(MessageHandler)
-):
-    await websocket.accept()
-    await message_handler.getSavedMessage(room_id=room_id, websocket=websocket)
-    await message_handler.subscribe_message(room_id=room_id, websocket=websocket)
-# ws = new WebSocket("ws://127.0.0.1:8000/HLChat/room_001");
+@app.get("/")
+def health_check_handler():
+    return {"ping": "pong"}
