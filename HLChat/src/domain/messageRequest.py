117a23594e13a9fa6310e795f7e396b74d4b698d
jaeheon.na <jaeheon.na@jaeheonna-MacBook-Pro.local>
2026-01-08 17:50:36 +0900

    gitignore

--- HLChat/src/domain/messageRequest.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class SendMessageRequest(BaseModel):
-    room_id: int | None = None
-    sender_id: str
-    receiver_id: list
-    message: str
-    message_type: str
-
