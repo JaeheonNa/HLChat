227a88c7ed8e9c3e5620f6a6cd8ce250aa294c01
jaeheon.na <jaeheon.na@jaeheonna-MacBook-Pro.local>
2026-01-08 17:52:45 +0900

    gitignore

--- HLChat/src/application/port/output/roomPort.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from domain.messageRequest import SendMessageRequest
-
-
-class MongoRoomPort(ABC):
-    @abstractmethod
-    async def save_room(self, request: SendMessageRequest) -> int:
-        pass
